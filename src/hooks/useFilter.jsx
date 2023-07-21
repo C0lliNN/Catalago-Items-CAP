2d1abeb77522cef4d578d8b44704eb2b7832ce4e
Raphael Collin <gcollin65@gmail.com>
2023-07-21 13:56:48 -0300

    Update useFilter.jsx

--- src/hooks/useFilter.jsx
+++ src/hooks/useFilter.jsx
@@ -4,11 +4,19 @@ import { useFetch } from "./useFetch";
 const url =
   "https://raw.githubusercontent.com/Josuerx12/josuerx12/main/Base%20para%20o%20cat%C3%A1logo.json";
 
+// This can a constant or you can add a dropdown (and useState) in the UI allowing the user to choose dynamically the page size.
+const PAGE_SIZE = 20;
+
 export const useFilter = () => {
   const { data, loading, error } = useFetch(url);
   const [code, setCode] = useState("");
   const [desc, setDesc] = useState("");
   const [category, setCategory] = useState("");
+  const [page, setPage] = useState(1);
+
+  const startOffset = (page - 1) * PAGE_SIZE;
+  // This will be useful for generating pagination buttons that will go from 1 to totalPages.
+  const totalPages = data.length / PAGE_SIZE;
 
   const filteredProducts =
     loading === false &&
@@ -27,7 +35,10 @@ export const useFilter = () => {
         (produto) =>
           !category ||
           produto.Categoria.toLowerCase().includes(category.toLowerCase())
-      );
+      )
+      .slice(startOffset, PAGE_SIZE)
 
-  return { filteredProducts, setCode, setDesc, setCategory, loading, error };
+  // As you can see, this hook is doing a lot of things already
+  // You can extract this logic into a custom hook called usePagination(dataAarray) 
+  return { filteredProducts, setCode, setDesc, setCategory, loading, error, page, setPage, totalPages, };
 };
